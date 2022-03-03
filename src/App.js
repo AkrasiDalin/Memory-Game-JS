7f560017483987869cda4dafd15184ea4fe14859
AkDalin <dalin.jo@yahoo.com>
2022-03-03 22:26:57 +0000

    Build prototype

--- src/App.js
+++ src/App.js
@@ -2,22 +2,31 @@ import logo from './logo.svg';
 import './App.css';
 
 function App() {
+  let previous = null;
+
+  function flip(ob) {
+    let target = ob.target;
+    console.log(target.innerText)
+    target.style.background == 'green' ? target.style.background = 'red' : target.style.background = 'green';
+    if(previous !== null && previous !== target){
+      console.log('yes',previous.innerText === target.innerText)
+      target.style.display = 'none';
+      previous.style.display = 'none';
+    }
+    else {
+      previous = target
+      console.log('no',previous.innerText === target.innerText)
+    }
+  }
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className="App">      
+      <div className="cardo" onClick={flip} style={{width: "100px", height:"200px", background: "red", border: "1px solid blue"}}>
+        ciao
+      </div>
+      <div className="cardo" onClick={flip} style={{width: "100px", height:"200px", background: "red", border: "1px solid blue"}}>
+        ciao
+      </div>
     </div>
   );
 }
